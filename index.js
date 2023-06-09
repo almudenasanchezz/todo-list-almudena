7e43364264e8df441dd4ecdea1f3a3b38b8ed49a
almudenasanchezz <sanchezgallegoalmudena@gmail.com>
2023-06-09 11:01:55 +0200

    errors fixed and added toggle filterActiveClass

--- index.js
+++ index.js
@@ -17,75 +17,79 @@ const testData = [
   },
 ];
 
-/* creamos una función que crea el contenedor de cada tarea, con su variable tarea */
-/* esta función se crea para poder utilizarla en la función renderList */
 function buildTask(task) {
-  /* declaramos el contenedor que contenga la tarea */
-  /* createElement nos ha creado un div dentro del html */
   const taskContainer = document.createElement("div");
-  /* creamos una clase para la constate creada anteriormente */
   taskContainer.className = "task__task";
-  /* dentro del div metemos el checkbox de la tarea con su descripción */
-  /* lo que tenemos a partir del dollar se lee de la siguiente manera: si la variable task está completada tendrá el valor checked, sino no tendrá ningún valor */
-  /* lo que está dentro de las llaves del dolar se interpreta como variable */
   taskContainer.innerHTML = `<input type="checkbox" ${
     task.isCompleted ? "checked" : ""
   } />${task.description}`;
   return taskContainer;
 }
 
-/* creamos una función que pinta en pantalla lo creado en la función anterior */
 function renderList(filterToApply) {
-  // filterToApply puede ser active, completed, all
-  /* crea una constante que selecciona el id del contenedor de tareas */
-  const taskNode = document.querySelector("#task-list");
-  /* por cada elemento del array se ejecuta una función con el parámetro tarea, que su valor es el de los objetos del array, un id, una descripcion y un booleano */
-  /* esto se usa para limpiar todas las tareas al cambiar de un filtro a otro y que no se vayan acumulando */
+  const tasksNode = document.querySelector("#task-list");
   tasksNode.innerHTML = "";
 
-  /* me creo una nueva variable que almacena las tareas que se van a mostrar y solo esas, es una variable que puedo modificar */
   let tasksToShow = testData;
 
-  /* active es el data-filter=active del html */
   if (filterToApply === "active") {
-    /* la funcion .filter funciona igual que el forEach, hace un recorrido del array y si devuelve verdadero se mete en un nuevo array y se devuelve falso no se mete en ninguno. Si inguno se cumple devuelve un array vacío. */
     tasksToShow = testData.filter(function (task) {
       return !task.isCompleted;
     });
   }
 
+  if (filterToApply === "completed") {
+    tasksToShow = testData.filter(function (task) {
+      return task.isCompleted;
+    });
+  }
+
   tasksToShow.forEach(function (task) {
-    /* se declara una constante cuyo valor es la función anterior (la estructura del checkbox) para poder pintar en pantalla */
     const taskHtml = buildTask(task);
-    /* pinta en el contenedor de tareas del html la tarea (taskHtml) */
-    taskNode.append(taskHtml);
+    tasksNode.append(taskHtml);
   });
 }
 
-/* declaramos constante cuyo valor es el todo lo que está dentro de la etiqueta "form" de html */
 const formNode = document.querySelector("#add-task");
-/* añadimos un evento que escucha al botón submit del formulario y ejecuta una función */
-/* a la función del event listener se le llama por defecto event */
 formNode.addEventListener("submit", function (event) {
-  /* se pone preventDefault para que al rellenar el formulario no me refresque la página porque por defecto se refresca */
   event.preventDefault();
-  /* para acceder al valor del input */ /* duda: no entiendo el add-task ni el description, si add-task es un id porque no se pone un # */ const description =
-    document.forms["add-task"]["description"].value;
-  /* creamos constante que lleve el id, description y isCompleted de cada tarea nueva que escribamos */
+  const description = document.forms["add-task"]["description"].value;
   const newTask = {
-    /* utilizamos Date.getTime para crear id únicos */
     id: new Date().getTime(),
     description,
-    /* para que por defecto salga como sin completar */
     isCompleted: false,
   };
 
-  /* hacemos esto al igual que antes para pintar en pantalla la nueva tarea */
   const taskHtml = buildTask(newTask);
-  /* volvemos a definir la constante taskNode para que funcione dentro de esta funcion */
-  const taskNode = document.querySelector("#task-list");
-  taskNode.append(taskHtml);
+  const tasksNode = document.querySelector("#task-list");
+  tasksNode.append(taskHtml);
   formNode.reset();
 });
-/* llama a la función renderList */
+
+// Filters
+const filterButtons = document.querySelectorAll(".filter__option");
+const filterActiveClass = "filter__option--selected";
+filterButtons.forEach(function (filterButton) {
+  filterButton.addEventListener("click", function () {
+    if (this.classList.contains(filterActiveClass)) {
+      return;
+    }
+
+    const filterToApply = this.getAttribute("data-filter");
+    renderList(filterToApply);
+
+    // remove class
+    document
+      .querySelector(`.${filterActiveClass}`)
+      .classList.remove(filterActiveClass);
+    this.classList.add(filterActiveClass);
+
+    if (filterToApply === "completed") {
+      document.querySelector("#add-task").classList.add("hidden");
+    } else {
+      document.querySelector("#add-task").classList.remove("hidden");
+    }
+  });
+});
+
 renderList();
